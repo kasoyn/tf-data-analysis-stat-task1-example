2a3788e39296fc9fe80bc4e42a900be936c3deb5
kasoyn <36932825+kasoyn@users.noreply.github.com>
2023-04-08 04:34:58 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 297816265 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    def log_likelihood(A, data):
+        return np.sum(-A*26 + data*np.log(A*26) - np.log(np.math.factorial(data)))
+    from scipy.optimize import minimize
+    A_hat = minimize(lambda A: -log_likelihood(A, np.sum(x)), x0=1, method='BFGS').x[0] / 26
+    
+    mse = []
+    for n in [10, 100, 1000]:
+        sample = np.random.choice(x, size=n, replace=False)
+        mse.append(np.mean((A_hat*n - sample)**2))
+    
+    thresholds = [0.00461, 0.00121, 0.000466]
+    score = sum([1 if mse[i] < thresholds[i] else 0 for i in range(3)])
+
+    return A_hat
